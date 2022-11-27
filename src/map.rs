83805da9994cbed1bb1f870f26aac4663dbeaf3c
Artur Serra <artur_seilah@hotmail.com>
2022-11-26 22:10:03 -0300

    Implemented the map function and added it to main

--- src/map.rs
+++ src/map.rs
@@ -1,2 +1,45 @@
 use crate::prelude::*;
 const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+pub fn map_index(x: i32, y: i32) -> usize {
+    ((y * SCREEN_WIDTH) + x) as usize
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+
+    pub fn render(&self, ctx: &mut BTerm) {
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let index = map_index(x, y);
+
+                match self.tiles[index] {
+                    TileType::Wall => {
+                        ctx.set(x, y, GRAY, BLACK,
+                            to_cp437('#'));
+                    }
+
+                    TileType::Floor => {
+                        ctx.set(x, y, YELLOW, BLACK,
+                            to_cp437('.')
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
