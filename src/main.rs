f930ed3f0fc981785905f5a344b230066cff3d6d
Artur Serra <artur_seilah@hotmail.com>
2022-12-02 23:12:54 -0300

    Added a graphical tileset as a font file, updated main to run with specified dimensions and call the font file

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
     pub use crate::map::*;
     pub use crate::player::*;
     pub use crate::map_builder::*;
@@ -41,9 +43,15 @@ impl GameState for State {
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50()
+    let context = BTermBuilder::new()
         .with_title("The Siege of Tuhr")
         .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("tuhr_font_file_prod.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "tuhr_font_file_prod.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "tuhr_font_file_prod.png")
         .build()?;
 
         main_loop(context, State::new())
