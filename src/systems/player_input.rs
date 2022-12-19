c8395a9924feaa80998d2d53bedaeaffc551ddae
Artur Serra <artur_seilah@hotmail.com>
2022-12-18 21:26:15 -0300

    Added the turn based system and refactored to hold these systems

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -8,7 +8,8 @@ pub fn player_input(
     ecs: &mut SubWorld,
     #[resource] map: &Map,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera
+    #[resource] camera: &mut Camera,
+    #[resource] turn_state: &mut TurnState
     ) {
     if let Some(key) = key {
         let delta = match key {
@@ -29,6 +30,7 @@ pub fn player_input(
                 if map.walkable_path(destination) {
                     *pos = destination;
                     camera.on_player_move(destination);
+                    *turn_state = TurnState::PlayerTurn;
                 }
             });
         }
